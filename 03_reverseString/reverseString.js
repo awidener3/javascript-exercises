6e0d3ebc1bdcf7c06ddf3705200bb601e50097f4
Alex Widener <awidener3@gmail.com>
2021-10-14 20:29:16 -0400

    solved reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
 const reverseString = function(str) {
+    let wordPieces = [];
 
+    for (let i = 0; i < str.length; i++) {
+        wordPieces.push(str[i]);
+    }
+    return wordPieces.reverse().join('');
 };
 
 // Do not edit below this line
