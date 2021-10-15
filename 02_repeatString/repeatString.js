5967126c9ef36289d576bcba1d9b6b7f4a4a3fad
Alex Widener <awidener3@gmail.com>
2021-10-14 20:08:22 -0400

    solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    
+    // repeated string with .repeat()
+    
+    return string.repeat(num);
+    
+    // repeated string with a for loop
+    
+    /*const repeatedString = [];
+    for (i = 1; i <= num; i++) {
+        repeatedString.push(string);
+    }
+    return repeatedString.join('');*/
+    
 };
 
 // Do not edit below this line
