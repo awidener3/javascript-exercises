e92b64aecf481806318ed79ddf5b61e476691c13
Alex Widener <awidener3@gmail.com>
2021-10-15 09:36:18 -0400

    solved removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...num) {
+    let args = Array.from(num);
+    console.log(args);
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            console.log(arr[i] + ' ' + args[j]);
+            if (arr[i] === args[j]) {
+                console.log('remove ' + arr[i]);
+                arr.splice(i, 1);
+                // addresses a glitch where .splice() skips the next iteration
+                i--;
+            }
+            console.log('you stay');
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
