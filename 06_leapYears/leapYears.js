e990030efbf5b6fa7847aaff0a2be8b9bc1e872f
Alex Widener <awidener3@gmail.com>
2021-10-19 09:42:56 -0400

    solved 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+// Leap years are years divisible by four. However, years divisible by 100 are not leap years, unless they are divisible by 400,
 
+const leapYears = function(year) {
+    // check if year is divisble by 4 and not divisible by 100
+    if (year % 400 === 0) return true;
+    if (year % 4 === 0 && year % 100 !== 0) {
+        // if true, return true
+        return true;
+    } else {
+        // otherwise return false
+        return false;
+    }
 };
 
 // Do not edit below this line
